2314838f3b570b61200fb7b632901a6f7f21c535
Frank Cwitkowitz <cwitkowitz@gmail.com>
2023-02-06 13:29:21 -0500

    Implemented initial training loop with decreasing loss observed.

--- train.py
+++ train.py
@@ -6,6 +6,7 @@ from model import PlaceHolderNet
 from NSynth import NSynth
 
 # Regular imports
+from torch.utils.tensorboard import SummaryWriter
 from torch.utils.data import DataLoader
 
 import torch
@@ -21,28 +22,54 @@ dataset_base_dir = os.path.join(os.path.expanduser('~'), 'Desktop', 'Datasets', 
 
 nsynth = NSynth(base_dir=dataset_base_dir, seed=seed)
 
-loader = DataLoader(nsynth, batch_size=10, shuffle=True, num_workers=0, drop_last=False)
+batch_size = 10
+loader = DataLoader(nsynth, batch_size=batch_size, shuffle=True, num_workers=0, drop_last=True)
 
 model = PlaceHolderNet()
+
+# Initialize an optimizer for the model parameters
+optimizer = torch.optim.Adam(model.parameters(), lr=1E-4)
+
+writer = SummaryWriter()
+
+iter = 0
 
 while True:
     for batch in loader:
         isolated_embeddings = model(batch)
 
-        mixtures = batch.unsqueeze(0).repeat(10, 1, 1) + \
-                   batch.unsqueeze(1).repeat(1, 10, 1)
+        mixtures = batch.unsqueeze(0).repeat(batch_size, 1, 1) + \
+                   batch.unsqueeze(1).repeat(1, batch_size, 1)
 
-        mixture_idcs_r = torch.arange(10).unsqueeze(0).repeat(10, 1).reshape(10 * 10)
-        mixture_idcs_c = torch.arange(10).unsqueeze(1).repeat(1, 10).reshape(10 * 10)
+        mixture_idcs_r = torch.arange(batch_size).unsqueeze(0).repeat(batch_size, 1).flatten()
+        mixture_idcs_c = torch.arange(batch_size).unsqueeze(1).repeat(1, batch_size).flatten()
 
         # TODO - try random mixtures instead of pairwise?
 
-        mixtures = mixtures.reshape(10 * 10, -1)
+        mixtures = mixtures.reshape(batch_size ** 2, -1)
 
         mixtures = mixtures / 2
 
         mixture_embeddings = model(mixtures)
 
-        # TODO - compute loss between isolated and mixed embeddings
+        pair_weights = 1 - torch.eye(batch_size).flatten()
 
-        pass
+        target_embeddings = isolated_embeddings[mixture_idcs_r] + \
+                            isolated_embeddings[mixture_idcs_c]
+
+        pair_losses = torch.nn.functional.mse_loss(mixture_embeddings, target_embeddings, reduction='none')
+
+        loss = torch.mean(pair_weights * pair_losses.sum(-1).mean(-1)) / 2
+
+        writer.add_scalar('train/loss', loss, iter)
+
+        print(f'iter: {iter} | loss: {loss}')
+
+        # Zero the accumulated gradients
+        optimizer.zero_grad()
+        # Compute gradients based on total loss
+        loss.backward()
+        # Perform an optimization step
+        optimizer.step()
+
+        iter += 1
