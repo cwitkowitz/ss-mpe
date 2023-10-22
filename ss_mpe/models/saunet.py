d5ce2b0a7a474e6b76e479aaabb8c8b8eb7d9a3d
Frank Cwitkowitz <cwitkowitz@gmail.com>
2023-10-22 11:40:17 -0400

    Refreshed code for SA U-Net variant.

--- ss_mpe/models/saunet.py
+++ ss_mpe/models/saunet.py
@@ -81,6 +81,93 @@ class AE_Base(SS_MPE):
         return output, latents, losses
 
 
+class AE_SA(AE_Base):
+    """
+    Implements adaptation of U-Net with self-attention from https://ieeexplore.ieee.org/document/9865174.
+    """
+
+    def __init__(self, hcqt_params, model_complexity=1, skip_connections=True, max_seq_len=None):
+        """
+        Initialize the full autoencoder with bottleneck components.
+
+        Parameters
+        ----------
+        See AE_Base class for others...
+
+        max_seq_len : int
+          Upper boundary for sequence lengths sampled during training
+        """
+
+        super().__init__(hcqt_params, model_complexity, skip_connections)
+
+        # Embedding sizes for self-attention modules
+        d_attention = 8 * 8 * 2 ** (model_complexity - 1)
+        d_forward = 1024 * 2 ** (model_complexity - 1)
+
+        self.positional = SinusoidalEncodings(max_seq_len=max_seq_len)
+
+        self.bottleneck_in = nn.TransformerEncoderLayer(d_model=d_attention,
+                                                        nhead=8,
+                                                        dim_feedforward=d_forward,
+                                                        batch_first=True)
+        self.bottleneck_out = nn.TransformerEncoderLayer(d_model=d_attention,
+                                                         nhead=8,
+                                                         dim_feedforward=d_forward,
+                                                         batch_first=True)
+
+    def forward(self, features):
+        """
+        Process spectral features to obtain pitch salience logits (for training/evaluation).
+
+        Parameters
+        ----------
+        features : Tensor (B x H x F x T)
+          Batch of HCQT spectral features
+
+        Returns
+        ----------
+        output : Tensor (B x F X T)
+          Batch of (implicit) pitch salience logits
+        latents : Tensor (B x 8 * H x F_lat, T_lat)
+          Batch of latent codes
+        losses : dict containing
+          ...
+        """
+
+        # Normalize the spectral features
+        features = self.layernorm(features.permute(0, -1, -3, -2)).permute(0, -2, -1, -3)
+
+        # Process features with the encoder
+        latents_, padding, embeddings, losses = self.encoder(features)
+
+        # Keep track of dimensionality before the bottleneck
+        dimensionality = latents_.size()
+        # Flatten frequency/time dimensions and swap channel dimension
+        latents_ = latents_.flatten(-2).transpose(-1, -2)
+        # Add positional encodings to embeddings
+        latents_ = self.positional(latents_)
+        # Apply first self-attention bottleneck
+        latents_ = self.bottleneck_in(latents_)
+        # Restore original dimensionality to obtain latents
+        latents = latents_.transpose(-1, -2).view(dimensionality)
+        # Apply second self-attention bottleneck
+        latents_ = self.bottleneck_out(latents_)
+        # Restore original dimensionality in preparation for decoder
+        latents_ = latents_.transpose(-1, -2).view(dimensionality)
+
+        if not self.skip_connections:
+            # Discard embeddings from encoder
+            embeddings = [None for _ in embeddings]
+
+        # Process latents with the decoder
+        output = self.decoder(latents_, padding, embeddings)
+
+        # Collapse channel dimension
+        output = output.squeeze(-3)
+
+        return output, latents, losses
+
+
 class Encoder(nn.Module):
     """
     Implements a 2D convolutional encoder.
@@ -436,7 +523,7 @@ class ConcatenativeUpSample2d(nn.Module):
         x : Tensor (B x C_in x H x W)
           Batch of input features
         padding : tuple of (int, int)
-          Padding along frequency and time dimension
+          Padding along frequency/time dimension
 
         Returns
         ----------
@@ -458,208 +545,83 @@ class ConcatenativeUpSample2d(nn.Module):
 
         return y
 
-# TODO - working through the following
-
-
-class AE_SA(SS_MPE):
-    """
-    SA U-Net adapted from https://github.com/christofw/multipitch_architectures (simple_u_net_doubleselfattn).
-    TODO - better description
-    """
-
-    def __init__(self, n_ch_in=6, n_bins_in=216, n_heads=8, model_complexity=1):
-        """
-        TODO
-        """
-
-        nn.Module.__init__(self)
-
-        # Number of channels at each stage
-        n_ch_1 = 8 * 2 ** (model_complexity - 1)
-        n_ch_2 = 16 * 2 ** (model_complexity - 1)
-        n_ch_3 = 32 * 2 ** (model_complexity - 1)
-        n_ch_4 = 64 * 2 ** (model_complexity - 1)
-
-        # Embedding sizes for self-attention modules
-        d_attention = 8 * 8 * 2 ** (model_complexity - 1)
-        d_forward = 1024 * 2 ** (model_complexity - 1)
-
-        # Layer normalization over frequency and channels (harmonics of HCQT)
-        self.layernorm = nn.LayerNorm(normalized_shape=[n_ch_in, n_bins_in])
-
-        self.initial_conv = DoubleConv(in_channels=n_ch_in,
-                                       out_channels=n_ch_1,
-                                       kernel_size=15,
-                                       padding=7)
-
-        self.down_block_1 = nn.Sequential(
-            nn.MaxPool2d(kernel_size=2),
-            DoubleConv(in_channels=n_ch_1,
-                       out_channels=n_ch_2,
-                       kernel_size=15,
-                       padding=7)
-        )
-        self.down_block_2 = nn.Sequential(
-            nn.MaxPool2d(kernel_size=2),
-            DoubleConv(in_channels=n_ch_2,
-                       out_channels=n_ch_3,
-                       kernel_size=9,
-                       padding=4)
-        )
-        self.down_block_3 = nn.Sequential(
-            nn.MaxPool2d(kernel_size=2),
-            DoubleConv(in_channels=n_ch_3,
-                       out_channels=n_ch_4,
-                       kernel_size=5,
-                       padding=2)
-        )
-        self.down_block_4 = nn.Sequential(
-            nn.MaxPool2d(kernel_size=2),
-            DoubleConv(in_channels=n_ch_4,
-                       out_channels=n_ch_4,
-                       kernel_size=3,
-                       padding=1)
-        )
-
-        self.positional = SinusoidalEncodings()
-
-        self.bottleneck = nn.Sequential(
-            nn.TransformerEncoderLayer(d_model=d_attention,
-                                       nhead=n_heads,
-                                       dim_feedforward=d_forward,
-                                       batch_first=True),
-            nn.TransformerEncoderLayer(d_model=d_attention,
-                                       nhead=n_heads,
-                                       dim_feedforward=d_forward,
-                                       batch_first=True)
-        )
-
-        self.concat_up = ConcatenativeUpSample2d(factor=2)
-
-        self.up_conv_1 = DoubleConv(in_channels=2 * n_ch_4,
-                                    mid_channels=n_ch_4,
-                                    out_channels=n_ch_3,
-                                    kernel_size=3,
-                                    padding=1)
-        self.up_conv_2 = DoubleConv(in_channels=2 * n_ch_3,
-                                    mid_channels=n_ch_3,
-                                    out_channels=n_ch_2,
-                                    kernel_size=5,
-                                    padding=2)
-        self.up_conv_3 = DoubleConv(in_channels=2 * n_ch_2,
-                                    mid_channels=n_ch_2,
-                                    out_channels=n_ch_1,
-                                    kernel_size=9,
-                                    padding=4)
-        self.up_conv_4 = DoubleConv(in_channels=2 * n_ch_1,
-                                    mid_channels=n_ch_1,
-                                    out_channels=1,
-                                    kernel_size=15,
-                                    padding=7,
-                                    final_layer=True)
-
-    def forward(self, hcqt, max_seq=None):
-        """
-        TODO
-        """
-
-        # Normalize harmonic channels and frequency bins of HCQTs
-        hcqt = self.layernorm(hcqt.transpose(-1, -2).transpose(-2, -3)
-                              ).transpose(-2, -3).transpose(-1, -2)
-
-        # Obtain an initial set of features
-        x1 = self.initial_conv(hcqt)
-
-        # Feed features through all downsampling blocks
-        x2 = self.down_block_1(x1)
-        x3 = self.down_block_2(x2)
-        x4 = self.down_block_3(x3)
-        embeddings = self.down_block_4(x4)
-
-        # Keep track of dimensionality before the bottleneck
-        dimensionality = embeddings.size()
-
-        # Flatten time and frequency dimensions and switch with channel dimension
-        embeddings = embeddings.flatten(-2).transpose(-1, -2)
-
-        # Feed the features through the self-attention bottleneck
-        embeddings = self.bottleneck(self.positional(embeddings, max_seq))
-
-        # Restore original dimensions to the embeddings
-        embeddings = embeddings.transpose(-1, -2).view(dimensionality)
-
-        # Feed features through all upsampling blocks
-        x = self.up_conv_1(self.concat_up(embeddings, x4))
-        x = self.up_conv_2(self.concat_up(x, x3))
-        x = self.up_conv_3(self.concat_up(x, x2))
-        salience = self.up_conv_4(self.concat_up(x, x1))
-
-        return salience
-
 
 class SinusoidalEncodings(nn.Module):
     """
-    Module to add fixed (sinusoidal) positional encoding to embeddings.
+    Module to add fixed (sinusoidal) positional encodings to a sequence of embeddings.
     """
 
-    def __init__(self, upper_bound=10000, interleave=True):
+    def __init__(self, max_period=10000, interleave=True, max_seq_len=None):
         """
-        Initialize the module.
+        Initialize the encoding module.
 
         Parameters
         ----------
-        upper_bound : int or float
+        max_period : int or float
           Upper boundary for geometric progression of periods (in 2π radians)
         interleave : bool
-          TODO
+          Whether to interleave sine and cosine encodings along feature dimension
+        max_seq_len : int
+          Upper boundary for sequence lengths sampled during training
         """
 
         nn.Module.__init__(self)
 
-        self.upper_bound = upper_bound
+        self.max_period = max_period
         self.interleave = interleave
+        self.max_seq_len = max_seq_len
 
-    def forward(self, features, max_seq=None):
+    def forward(self, x):
         """
-        TODO
+        Add positional encodings to a sequence of embeddings.
+
+        Parameters
+        ----------
+        x : Tensor (B x N x D)
+          Batch of input features
+
+        Returns
+        ----------
+        y : Tensor (B x N x D)
+          Batch of corresponding output features
         """
 
-        # Determine the dimensionality of the input features
-        batch_size, seq_length, d_model = features.size()
+        # Obtain dimensionality of input features
+        batch_size, seq_length, d_model = x.size()
 
-        # Determine the periods of the sinusoids
-        periods = self.upper_bound ** (torch.arange(0, d_model, 2) / d_model)
-        # Construct a tensor of position indices for the features
-        positions = torch.arange(0, seq_length)
+        # Compute period of sinusoid corresponding to each feature dimension
+        periods = self.max_period ** (torch.arange(0, d_model, 2, device=x.device) / d_model)
+        # Construct a tensor of position indices
+        positions = torch.arange(0, seq_length, device=x.device)
 
-        if self.training and max_seq is not None:
-            if max_seq > seq_length + 1:
-                # Add a random offset to diversify positions
-                positions += torch.randint(high=(max_seq - seq_length + 1), size=(1,))
+        if self.training and self.max_seq_len is not None:
+            # Determine highest possible start position
+            max_pos = self.max_seq_len - seq_length
 
-        # Multiply every position by every period
+            if max_pos > 0:
+                # Add random offset to positions to augment positional encodings
+                positions += torch.randint(high=max_pos + 1, size=(1,), device=x.device)
+
+        # Multiply every position by every frequency
         angles = torch.outer(positions, 1 / periods)
 
-        # Compute the sine and cosine of the angles
+        # Compute sine and cosine of angles
         angles_sin = torch.sin(angles)
         angles_cos = torch.cos(angles)
 
         if self.interleave:
             # Add an extra dimension to each vector
-            angles_sin = torch.unsqueeze(angles_sin, dim=-1)
-            angles_cos = torch.unsqueeze(angles_cos, dim=-1)
+            angles_sin = angles_sin.unsqueeze(-1)
+            angles_cos = angles_cos.unsqueeze(-1)
 
-        # Interleave the two positional encodings
+        # Combine the two sets of positional encodings
         sinusoidal_encodings = torch.cat((angles_sin, angles_cos), dim=-1)
 
         if self.interleave:
-            # Collapse the added dimension to interleave the vectors
+            # Collapse added dimension to interleave the two sets of positional encodings
             sinusoidal_encodings = sinusoidal_encodings.view(seq_length, d_model)
 
-        # Add the encodings to the appropriate device
-        sinusoidal_encodings = sinusoidal_encodings.to(features.device)
+        # Repeat encodings for each sample in batch and add to embeddings
+        y = x + torch.tile(sinusoidal_encodings, (batch_size, 1, 1))
 
-        # Repeat encodings for each sample in the batch and add to appropriate device
-        features = features + torch.tile(sinusoidal_encodings, (batch_size, 1, 1))
-
-        return features
+        return y
